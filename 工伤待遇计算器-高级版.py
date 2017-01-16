f3fe542fd8e777eca5374c7525e376f7bd3c040c
zhengty158 <zhengty158@users.noreply.github.com>
2017-01-16 19:23:00 +0800

    Update 工伤待遇计算器-高级版.py

--- 工伤待遇计算器-高级版.py
+++ 工伤待遇计算器-高级版.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 # 工伤待遇计算器
-# 已将所有代码尽量精简为一个函数，尽量实现代码模块化
-# 已改写变量名，增强代码可读性
 # 该代码适合律师、法务、人力资源管理人员使用
 # 该代码中某些基数使用广州市相关标准（比如上年度职工平均工资），而且是2016年度的，各位使用时请根据所在地区及当年最新相关标准修改
-# 还需增加：输出计算结果为txt文档的功能
 
-# 这一版删除了按提示输入工伤情况数据的代码，将其改为由参数获取，然后直接得到计算结果
-# DengJi = 工伤等级   SiFou_JieChu = 是否解除合同（1=是，2=否）  GongZhi_1 = 受伤前12个月平均工资  GongZhi_2 = 解除劳动合同前12个月平均工资
+# 参数说明：
 
-def jsq(DengJi, SiFou_JieChu, GongZhi_1, ZhuYuan_TianShu, HuLi_TianShu, TingGongLiuXinQi_YueShu,
+# Deng_Ji = 工伤等级   SiFou_JieChu = 是否解除合同（1=是，2=否）  GongZhi_1 = 受伤前12个月平均工资
+
+# ZhuYuan_TianShu = 住院天数   HuLi_TianShu = 护理天数   TingGongLiuXinQi_YueShu = 停工留薪期月数
+
+# TingGongLiuXinQi_TianShu = 停工留薪期天数   TingGongLiuXinQi_ShiFaGongZhi = 停工留薪期实发工资
+
+# GongZhi_2 = 解除劳动合同前12个月平均工资
+
+
+def jsq(Deng_Ji, SiFou_JieChu, GongZhi_1, ZhuYuan_TianShu, HuLi_TianShu, TingGongLiuXinQi_YueShu,
         TingGongLiuXinQi_TianShu, TingGongLiuXinQi_ShiFaGongZhi, GongZhi_2):
-    YuePing_GongZhi = int('6764')
-    HuoShiBuZhu_JiaGe = int('35')
+    YuePing_GongZhi = int('6764')   # 广州市2016年公布的上年度职工月平均工资
+    HuoShiBuZhu_JiaGe = int('35')   # 住院伙食补助标准
     # 告知用户，并限定工资计算基数在合法范围内：
     if SiFou_JieChu == 1:
         print('\n请注意计算基数的法定范围：\n2016年7月公布的广州职工平均工资6764元')
@@ -35,14 +40,14 @@ def jsq(DengJi, SiFou_JieChu, GongZhi_1, ZhuYuan_TianShu, HuLi_TianShu, TingGong
             GongZhi_2 = YuePing_GongZhi * 3
             print('\n解除劳动合同前12个月平均工资高于广州职工平均工资6764元的300%，\n本计算器将采用20292元作为计算基数！\n')
     # 根据用户输入的伤残等级、是否解除劳动合同，调用相应等级的费率及情况计算工伤待遇：  
-    DengJi -= 1
+    Deng_Ji -= 1
     Fei_Lv = [[0.6,27,0.9],[0.5,25,0.85],[0.4,23,0.8],[0.3,21,0.75],[18,10,50,0.7],[16,8,40,0.6],[13,6,25],[11,4,15],[9,2,8],[7,1,4]]
-    Fei_Lv1 = Fei_Lv[DengJi][0]
-    Fei_Lv2 = Fei_Lv[DengJi][1]
-    Fei_Lv3 = Fei_Lv[DengJi][2]
-    if DengJi == 4 or DengJi == 5:
+    Fei_Lv1 = Fei_Lv[Deng_Ji][0]
+    Fei_Lv2 = Fei_Lv[Deng_Ji][1]
+    Fei_Lv3 = Fei_Lv[Deng_Ji][2]
+    if Deng_Ji == 4 or Deng_Ji == 5:
         Fei_Lv4 = Fei_Lv[DengJi][3]
-    if (DengJi+1) > 4:
+    if (Deng_Ji+1) > 4:
         print('\n\n根据以上条件计算，工伤待遇为：\n')
         a = float('%.2f' % (GongZhi_1 * Fei_Lv1))
         print('一次性伤残补助金：', a, '元\n')
@@ -51,14 +56,13 @@ def jsq(DengJi, SiFou_JieChu, GongZhi_1, ZhuYuan_TianShu, HuLi_TianShu, TingGong
             c = float('%.2f' % (max(GongZhi_1, GongZhi_2) * Fei_Lv3))
             print('一次性工伤医疗补助金：', b, '元\n')
             print('一次性伤残就业补助金：', c, '元\n')   
-        if SiFou_JieChu == 2:
-            if DengJi == 4 or DengJi == 5:
+        if (SiFou_JieChu == 2) and (Deng_Ji == 4 or Deng_Ji == 5):
                 print('伤残津贴（每月）：',float('%.2f' % (GongZhi_1 * Fei_Lv4)),'元\n')
-    elif (DengJi+1) <= 4:
-        z = int(input('请输入生活自理障碍程度等级（数字）：'))
-        z -= 1
+    elif (Deng_Ji+1) <= 4:
+        ZiLi_ZhangAi = int(input('请输入生活自理障碍程度等级（数字）：'))
+        ZiLi_ZhangAi -= 1
         ShengHuoHuLiFei_DengJi_FeiLv = [0.6, 0.5, 0.4, 0.3]
-        temp1 = float('%.2f' % (YuePing_GongZhi * ShengHuoHuLiFei_DengJi_FeiLv[z]))
+        temp1 = float('%.2f' % (YuePing_GongZhi * ShengHuoHuLiFei_DengJi_FeiLv[ZiLi_ZhangAi]))
         print('\n\n根据以上条件计算，工伤待遇为：\n')
         print('生活护理费（每月）:', temp1, '元', '（生活护理费以上年广州职工平均工资为计算基数）\n')
         print('一次性伤残补助金：',float('%.2f' % (GongZhi_1 * Fei_Lv2)),'元\n')
@@ -71,13 +75,15 @@ def jsq(DengJi, SiFou_JieChu, GongZhi_1, ZhuYuan_TianShu, HuLi_TianShu, TingGong
     print('停工留薪期待遇（应发）：', f, '元\n')
     g = float('%.2f' % ((TingGongLiuXinQi_YueShu * GongZhi_1) + TingGongLiuXinQi_TianShu * (GongZhi_1 / 21.75) - TingGongLiuXinQi_ShiFaGongZhi ))
     print('停工留薪期待遇（补差额）：', g, '元\n')
-    if (DengJi+1) > 4:
-        if SiFou_JieChu == 1:
-            print('\n以上待遇应发总额为：', float('%.2f' % (a + b + c + d + e + f)), '元')
-            print('\n以上待遇补差总额为：', float('%.2f' % (a + b + c + d + e + g)), '元')
-        if SiFou_JieChu == 2:
-            print('\n以上待遇应发总额为：', float('%.2f' % (a + d + e + f)), '元')
-            print('\n以上待遇补差总额为：', float('%.2f' % (a + d + e + g)), '元')
-    if DengJi == 4 or DengJi == 5:
+    if ((Deng_Ji+1) > 4) and (SiFou_JieChu == 1):
+        print('\n以上待遇应发总额为：', float('%.2f' % (a + b + c + d + e + f)), '元')
+        print('\n以上待遇补差总额为：', float('%.2f' % (a + b + c + d + e + g)), '元')
+    elif ((Deng_Ji+1) > 4) and (SiFou_JieChu == 2):
+        print('\n以上待遇应发总额为：', float('%.2f' % (a + d + e + f)), '元')
+        print('\n以上待遇补差总额为：', float('%.2f' % (a + d + e + g)), '元')
+    elif (Deng_Ji == 4 or Deng_Ji == 5) and (SiFou_JieChu == 1):
         print('\n以上待遇应发总额为（不含伤残津贴）：', float('%.2f' % (a + b + c + d + e + f)), '元')
         print('\n以上待遇补差总额为（不含伤残津贴）：', float('%.2f' % (a + b + c + d + e + g)), '元')
+    elif (Deng_Ji == 4 or Deng_Ji == 5) and (SiFou_JieChu == 2):
+        print('\n以上待遇应发总额为（不含伤残津贴）：', float('%.2f' % (a + d + e + f)), '元')
+        print('\n以上待遇补差总额为（不含伤残津贴）：', float('%.2f' % (a + d + e + g)), '元')
