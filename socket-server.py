f3ea7167584b1edd42c339fee6e1f348209995f2
zhengty158 <zhengty158@users.noreply.github.com>
2017-02-18 16:16:11 +0800

    Update and rename socket.py to socket-server.py

--- socket-server.py
+++ socket-server.py
@@ -4,7 +4,7 @@
 import socket
 import sys
 
-HOST = ''
+HOST = ''                                    # 另一种写法：HOST = socket.gethostname()，但是需要在建好socket之后才有用
 PORT = 8888
 
 s = socket.socket()                          # socket.socket()默认参数是IPV4，TCP
@@ -24,8 +24,8 @@ print('Socket now listening')
 while True:                                   # 将True替换为1也可以
     conn, addr = s.accept()
 
-    print('Connected with ' + addr[0] + ':' + str(addr[1]))
-    print('the localpoint is ' + str(conn))
+    print('已连接的客户端地址：%s，端口号：%s' % (str(addr[0]), str(addr[1])))
+    print('服务器信息：%s' % str(conn))
 
     data = conn.recv(1024)
     reply = 'ok...' + data.decode()           # python3必须注意编码问题，此处data应从bytes转换为string
