1ec0a8fd5e4f35f27d9ba56f8cb9cb37dbb88414
zhengty158 <zhengty158@users.noreply.github.com>
2017-03-02 20:54:54 +0800

    Update 批量查找、删除文件--WIN.py

--- 批量查找、删除文件--WIN.py
+++ 批量查找、删除文件--WIN.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # 办公小工具：用Python调用Windows命令行，实现批量查找文件、删除文件功能
 # 自我感觉比Windows的bat（批处理）文件好用多了，不用每次都手动右键替换查询关键字和盘符，哈哈！顺便练练面向对象编程。
+# 为避免系统安全限制（比如：拒绝访问C盘），把《文件查找结果》.txt存放地址改为D盘
 
 import os
 
 class WIN_DIR_DEL(object):
 
-    CMD_dir = 'dir %s:\*%s /a/s/p >c:\\文件查找结果-%s.txt'  # WIN命令行的文件查找命令，>号后面是存放路径，用于存放查找结果
+    CMD_dir = 'dir %s:\*%s /a/s/p >d:\\文件查找结果-%s.txt'  # WIN命令行的文件查找命令，>号后面是存放路径，用于存放查找结果
     CMD_del = 'del %s:\*%s /a/s/p'                          # WIN命令行的文件删除命令，/a/s/p分别代表：查找所有属性的文件/查找所有子文件夹/每删除一个文件都需要手动确认
     All_PanFu = ['c', 'd', 'e', 'f']                        # WIN系统常见硬盘盘符
 
@@ -38,7 +39,7 @@ class WIN_DIR_DEL(object):
             else:
                 os.system(WIN_DIR_DEL.CMD_dir % (PanFu, WenJianMin, PanFu))
             
-        print('\n查找完成，可以到C盘查看《文件查找结果》\n如果没找到，C盘不会生成《文件查找结果》')
+        print('\n查找完成，可以到D盘查看《文件查找结果》\n如果没找到，D盘不会生成《文件查找结果》')
 
 
     # 以下函数为文件删除命令
@@ -78,7 +79,7 @@ class WIN_DIR_DEL(object):
         if choice == 1:
             WIN_DIR_DEL().WIN_dir()
             for x in WIN_DIR_DEL().All_PanFu:                  # 按顺序自动打开每一个《文件查找结果》.txt
-                os.system('c:\\文件查找结果-%s.txt' % x)
+                os.system('d:\\文件查找结果-%s.txt' % x)
                 input('\n按回车键查看下一个《文件查找结果》')
                 if x == list(WIN_DIR_DEL().All_PanFu)[-1]:
                     input('\n按回车键退出')
