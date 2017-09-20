7830f8691ac9548974ab58a17ec46c729744e7cc
zhengty158 <zhengty158@users.noreply.github.com>
2017-09-20 20:44:05 +0800

    Update 网络连接检查器.py

--- 网络连接检查器.py
+++ 网络连接检查器.py
@@ -1,6 +1,6 @@
 # 一个简单的网络连接检查工具
 
-import os
+import os, re
 
 output_ip = os.popen('ipconfig /all')
 output_ns = os.popen('nslookup')
@@ -14,10 +14,24 @@ if '8.8.8.8' in output_ip.read():
 else:
     print('DNS服务器已被篡改！！！')
 
+    
 # 以下使用ping命令检查网速和丢包率
 site = {'淘宝':'www.taobao.com', '腾讯':'www.qq.com', '百度':'www.baidu.com'}
 
 for k, v in site.items():
-    output = os.popen('ping %s' % v)
-    temp = output.readlines()[-3:]
-    print('\n%s连接速度：%s（平均）\n丢包率：%s%%' % (k, temp[-1][-5:-1], temp[-3][-8]))
+    output = os.popen('ping {website}'.format(website = v))
+    output_list = output.readlines()
+    keyword_1 = '平均 = (\d+)ms|Average = (\d+)ms'
+    keyword_2 = '(\d+)% 丢失|(\d+)% loss'
+    final_result = []
+    # 从ping命令结果列表中，提取平均连接时间和丢包率
+    for n in range(len(output_list)):
+        output_result_1 = re.findall(keyword_1, output_list[n])
+        if output_result_1 != []:
+            final_result.append(output_result_1[0][0] or output_result_1[0][1])
+        output_result_2 = re.findall(keyword_2, output_list[n])
+        if output_result_2 != []:
+            final_result.append(output_result_2[0][0] or output_result_2[0][1])
+    print('\n{website}连接速度：{time}毫秒（平均）\n丢包率：{percent}%'.format(website = k, time = final_result[1], percent = final_result[0]))
+
+input('\n\n完成')
