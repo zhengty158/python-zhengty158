003a009fbe143f8e9bb379a0f378f8ae70e9f893
zhengty158 <zhengty158@users.noreply.github.com>
2017-09-21 21:05:02 +0800

    Update 法律事务管理信息系统检测工具.py

--- 法律事务管理信息系统检测工具.py
+++ 法律事务管理信息系统检测工具.py
@@ -4,23 +4,18 @@ import os, webbrowser, time, re, urllib.request as req, random
 print('\n\n        法律事务管理信息系统检测工具\n\n')
 
 # 首先测试服务器是否正常运行
-print('1、测试法律系统服务器是否正常，请稍等......\n')
-ping = os.popen('ping 138.16.0.88')
-ping_result = ping.readlines()
-keyword_1 = '(\d+)% 丢失|(\d+)% loss'
-ping_loss = []
-for n in range(len(ping_result)):
-    loss = re.findall(keyword_1, ping_result[n])
-    if loss != []:
-        ping_loss.append(loss[0][0] or loss[0][1])
-        # 由于可能出现“Request timed out”等ping命令结果，因此不用平均连接时间来做判断
-print('法律系统服务器，本次连接数据包丢失率为{loss}%（平均）'.format(loss = ping_loss[0]))
+print('1、测试法律系统服务器是否正常，请稍等......')
+url_1 = req.Request('http://138.16.0.88')
 time.sleep(random.randint(1, 4))
-if ping_loss == ['0']:
-    print('\n测试结果：与服务器连接正常，服务器运行正常\n\n')
-    # 公司内部局域网连接速度一般为0ms，丢包率一般为0%
-else:
+try:
+    status_code = req.urlopen(url_1).getcode()
+    if status_code == 200:
+        print('\n服务器当前状态代码：200')
+        print('\n测试结果：与服务器连接正常，服务器运行正常\n\n')
+except u_err.URLError as error:
+    print('\n错误代码：', error.reason)
     print('\n测试结果：与服务器连接异常，服务器可能未正常运行，请联系科技部同事处理\n\n')
+
 time.sleep(2)
 
 
@@ -38,7 +33,9 @@ if '法律' in readlist[0]:
     print('\n-------------------------------------------------')
     input('\n\n测试结束，法律系统能正常使用')
 else:
-    print('测试结果：法律系统首页异常（无法显示）\n\n------------------------------------------------\n\n测试结束，法律系统不能正常使用\n\n现在打开服务器管理系统，可在此重启法律系统服务器.....')
+    print('测试结果：法律系统首页异常（无法显示）\n\n')
+    print('------------------------------------------------')
+    print('\n\n测试结束，法律系统不能正常使用\n\n现在打开服务器管理系统，可在此重启法律系统服务器.....')
     time.sleep(random.randint(5, 10))
     # 如果以上测试异常，就进入服务器管理系统重启法律系统服务器和Tomcat
     webbrowser.open('https://10.10.254.10/index')
